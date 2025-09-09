69aa7ef1d59796544776152e8b440202061fcc90
Shubham Auddy <shubhamauddy@gmail.com>
2025-09-09 23:44:04 +0530

    Added a test case in standAlone file

--- src/test/java/com/nopcommerce/tests/FooterStandAlone.java
+++ src/test/java/com/nopcommerce/tests/FooterStandAlone.java
@@ -1,5 +1,18 @@
 package com.nopcommerce.tests;
 
-public class FooterStandAlone {
+import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class FooterStandAlone extends BaseTest{
+	
+	@Test
+	public void verifyFotterBlockSections() {
+		List<WebElement> footerSections = driver.findElements(By.cssSelector(".footer-block .title"));
+		for(WebElement section: footerSections) {
+			System.out.println(section.getText());
+		}
+	}
 }
