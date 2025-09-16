47071e102df0826a5d52b62735667664de7a9c5f
Shubham Auddy <shubhamauddy@gmail.com>
2025-09-16 19:46:11 +0530

    Added test cases int the footer standalone file

--- src/test/java/com/nopcommerce/tests/FooterStandAlone.java
+++ src/test/java/com/nopcommerce/tests/FooterStandAlone.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -43,4 +44,53 @@ public class FooterStandAlone extends BaseTest{
 		}
 	}
 	
+	public void verifyFacebookButton() {
+		
+	}
+	
+	public void verifyXButton() {
+		
+	}
+	
+	public void verifyRSSButton() {
+		
+	}
+	
+	public void verifyYoutubeButton() {
+		
+	}
+	
+	public void verifyInstagramButton() {
+		
+	}
+	
+	public void verifyCompanyLink() {
+		
+	}
+	
+	@Test
+	public void verifyPositiveNewsLetterSubscription() {
+		driver.findElement(By.id("newsletter-email")).sendKeys("abcd@gmail.com");
+		driver.findElement(By.id("newsletter-subscribe-button")).click();
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("newsletter-result-block"))));
+		String expectedText = "Thank you for signing up! A verification email has been sent. We appreciate your interest";
+		String actualText = driver.findElement(By.id("newsletter-result-block")).getText();
+		Assert.assertEquals(actualText, expectedText);
+	}
+	
+	@Test
+	public void verifyNegetiveNewsLetterSubscription() {
+		driver.findElement(By.id("newsletter-email")).sendKeys("abcd");
+		driver.findElement(By.id("newsletter-subscribe-button")).click();
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("newsletter-result-block"))));
+		String expectedText = "Enter valid email";
+		String actualText = driver.findElement(By.id("newsletter-result-block")).getText();
+		Assert.assertEquals(actualText, expectedText);
+	}
+	
+	@Test
+	public void verifyCopyrightText() {
+		String footerCopyrightText = driver.findElement(By.cssSelector(".footer-disclaimer")).getText();
+		Assert.assertEquals(footerCopyrightText, "Copyright © 2025 nopCommerce demo store. All rights reserved.");
+	}
 }
